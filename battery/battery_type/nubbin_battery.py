2f36743df76c80fe5d4f5b10afee9487b956cb4a
Saathwik-2001 <sathwik.galimutti@gmail.com>
2023-08-30 22:28:44 +0530

    refactored codebase - added battery types - created new model for cars

---
+++ battery/battery_type/nubbin_battery.py
@@ -0,0 +1,12 @@
+from battery.battery import Battery
+
+class NubbinBattery(Battery):
+    def __init__(self, last_service_date,current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+    def needs_service(self):
+        service_year = self.last_service_date.year + 4
+        current_year = self.current_date.year
+        if(current_year>=service_year):
+            return True
+        else: return False
---
+++ engine/engine_type/capulet_engine.py
@@ -0,0 +1,12 @@
+from engine.engine import Engine
+
+class CapuletEngine(Engine):
+    def __init__(self,last_service_mileage, current_mileage):
+        self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
+        self.willoughby_milage_need_service = 60000
+
+    def needs_service(self):
+        if(self.current_mileage - self.last_service_mileage) > self.willoughby_milage_need_service:
+            return True
+        else: return False
